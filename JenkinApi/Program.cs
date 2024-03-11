caa4fcde73302929de592035502275ec58a395af
aliraza <aliraza@DESKTOP-SDMAHMT>
2024-03-11 10:50:40 +0400

    update

--- JenkinApi/Program.cs
+++ JenkinApi/Program.cs
@@ -17,13 +17,11 @@ namespace JenkinApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI();
-            //}
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseHttpsRedirection();
 
